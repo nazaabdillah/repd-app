d5bf89149c756bd7a3ab2e269e87bb12652561ba
Naza As Nza <pesnaza30maret@gmail.com>
2026-04-28 09:31:19 +0700

    error load fixed

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found, using environment variables")
 	}
 
 	config.ConnectDatabase()
